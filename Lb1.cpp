787b0e5b0ae301a5f8268ca7e6e93bd690cc94da
den.4fanasev.2000@gmail.com <den.4fanasev.2000@gmail.com>
2023-02-24 12:54:08 +0200

    0.2

--- Lb1.cpp
+++ Lb1.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+#include <cmath>
+#include "omp.h"
 using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "");
 
-	cout << "Українська мова" << endl;
+	const int size = (int)pow(1024, 3);
+	int* a = new int[size];
+	int* b = new int[size];
+	int* sum = new int[size];
+	for (int i = 0; i < size; i++) {
+		a[i] = 5;
+		b[i] = 5;
+	}
 }
