2bdb0dc8973f7f5c361ceb6e08224eaa476aeff0
den.4fanasev.2000@gmail.com <den.4fanasev.2000@gmail.com>
2023-02-25 18:28:14 +0200

    v 1.0

--- Lb1.cpp
+++ Lb1.cpp
@@ -3,22 +3,38 @@
 #include <omp.h>
 using namespace std;
 
+#define NUM_THREADS 16
+
 int main() {
 	setlocale(LC_ALL, "");
-	/*
+	
+	
 	const int size = (int)pow(1024, 3);
 	int* a = new int[size];
 	int* b = new int[size];
 	int* sum = new int[size];
-
+	double beg, end;
+	cout << "Start of initialization" << endl;
 	for (int i = 0; i < size; i++) {
 		a[i] = 5;
 		b[i] = 5;
 	}
-	*/
-	#pragma omp parallel num_threads(4)
+	cout << "Initialization's end" << endl;
+
+	omp_set_dynamic(0);
+	beg = omp_get_wtime();
+	#pragma omp parallel num_threads(NUM_THREADS)
 	{
-	cout << "Hi" << endl;
+		#pragma omp for
+		for (int i = 0; i < size; i++) {
+			a[i] = a[i] * b[i];
+		}
 	}
+	end = omp_get_wtime();
+
+	double t = end - beg;
+
+	cout << "Time: " << t << endl;
+
 
 }
